8ad4af68430fd60d3f314962cf47a23e6cedf3bf
Samuel S. Brito <samuel.ufop@gmail.com>
2014-09-09 09:55:11 -0300

    testing conflict detection

---
+++ src/osi_cgraph backup.cpp
@@ -0,0 +1,587 @@
+#include <cstring>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <algorithm>
+#include <climits>
+#include <OsiSolverInterface.hpp>
+#include <CoinPackedMatrix.hpp>
+#include "osi_cgraph.h"
+extern "C"
+{
+#include "memory.h"
+#include "node_heap.h"
+}
+
+#define oo  (INT_MAX/2)
+#define WORST_PRIORITY_ROW 10000   // to be considered
+
+using namespace std;
+
+#define EPS 1e-6
+
+/* mininum size for a row to be considered a clique row */
+#define MIN_CLIQUE_ROW 250
+
+#define IS_NEGATIVE_DBL( v ) ( v < -1e-5 )
+
+#define DBL_EQUAL( v1, v2 ) ( fabs(v1-v2) < EPS )
+
+// conflict vector dimensions
+#define CVEC_CAP   1800000
+#define CVEC_FLUSH 1700000
+
+#define MAX_NEGATIVE_COEFS 200
+
+const double LARGE_CONST = std::min( DBL_MAX/10.0, 1e20 );
+
+double maxRowTime = 0.0;
+int maxRowTimeIdx = -1;
+int activePairwise = 0, activeReadyToUse = 0, inactivePairwise = 0,
+    inactiveReadyToUse = 0, mixedPairwise = 0;
+int success = 1;
+
+struct sort_sec_pair
+{
+    bool operator()(const std::pair<int,int> &left, const std::pair<int,int> &right)
+    {
+        if ( left.second != right.second )
+            return left.second < right.second;
+
+        return left.first < right.first;
+    }
+};
+
+void processClique( const int n, const int *idx, CGraph *cgraph, vector< pair<int, int> > &cvec, const double *colLb, const double *colUb );
+
+/* if size is large enough fech conflicts */
+void fetchConflicts( vector< pair<int,int> > &cvec, const bool lastTime, CGraph *cgraph, vector<int> &neighs );
+
+/* returns how much a variable can negatively contribute */
+double mostNegativeContribution( const double coef, const char c, const double colLb, const double colUb );
+
+/* returns how much a variable can positively contribute */
+inline double unitaryContribution( const double coef, const char c, const double colLb, const double colUb );
+
+/* skip rows with large values to avoid overflows */
+bool canYeldLargeValue( const double coef, const double colLb, const double colUb );
+
+int compute_row_priority( OsiSolverInterface *lp, const int r );
+
+CGraph *osi_build_cgraph( void *_lp )
+{
+    clock_t start = clock(), end;
+    double cpuTime;
+
+    OsiSolverInterface *lp = (OsiSolverInterface *)_lp;
+
+    if (lp->getNumIntegers()<2)
+        return 0;
+
+    int cgraphSize = lp->getNumCols();
+#ifdef CONSIDER_BINARY_COMPLEMENT
+    cgraphSize *= 2;
+    //printf("cgraph: considering binary complement.\n");
+#endif
+    CGraph *cgraph = cgraph_create( cgraphSize );
+    const char *ctype = lp->getColType();
+    const CoinPackedMatrix *M = lp->getMatrixByRow();
+    const double *rhs = lp->getRightHandSide();
+    const double *colLb = lp->getColLower();
+    const double *colUb = lp->getColUpper();
+    const char *sense = lp->getRowSense();
+    const int nCols = lp->getNumCols();
+    const int nRows = lp->getNumRows();
+    int j;
+    int idxRow;
+
+    /* conflict vector */
+    vector< pair< int, int > > cvec;
+    cvec.reserve( CVEC_CAP );
+
+    /* used i fetch conflicts */
+    vector< int > neighs;
+    neighs.reserve( 8192 );
+
+    for(idxRow = 0; idxRow < nRows; idxRow++)
+    {
+        clock_t rowStart = clock();
+#define FIXED_IN_ZERO( idx ) ( (fabs(colLb[idx])<EPS) && (fabs(colUb[idx])<EPS) )
+        const CoinShallowPackedVector &row = M->getVector(idxRow);
+        const int *idx = row.getIndices();
+        const double *coefs = row.getElements();
+
+        double minCoef = numeric_limits<double>::max();
+        double maxCoef = -1.0 * numeric_limits<double>::max();
+        int nInts = 0; // number of integer variables
+        int nPos  = 0; // variables which may assume only zero or some positive value
+        int nNeg  = 0; // variables which may assume negative values
+
+        if ( (row.getNumElements()<2) || (fabs(rhs[idxRow])>=LARGE_CONST) )
+            continue;
+
+        end = clock();
+        cpuTime = ((double) (end - start)) / CLOCKS_PER_SEC;
+        if(cpuTime > 60.0)
+        {
+            success = 0;
+            break;
+        }
+
+        if ( sense[idxRow] == 'R' )  // lets not consider ranged constraints by now
+        {
+            //printf("TODO: CHECK FOR RANGED CONSTRAINT (%s) rhs is %g\n", lp->getRowName(idxRow).c_str(), rhs[idxRow] );
+            continue;
+        }
+
+#ifdef DEBUG_CONF
+        int newConflicts = 0;
+        vector< pair<int,int> > confS;
+        confS.clear();
+#endif /* DEBUG_CONF */
+
+#ifdef CONSIDER_BINARY_COMPLEMENT
+        /* inserting trivial conflicts: variable-complement */
+        for(j = 0; j < row.getNumElements(); j++)
+        {
+            const int cidx = idx[j];
+            cvec.push_back( pair<int, int>(cidx, cidx + nCols) );
+#ifdef DEBUG_CONF
+            newConflicts++;
+            confS.push_back( pair<int,int>(cidx, cidx + nCols) );
+#endif /* DEBUG_CONF */
+        }
+#endif /* CONSIDER_BINARY_COMPLEMENT */
+
+        /* checking number of integers and limits for variables */
+        double mult = 1.0;
+        if ( sense[idxRow] == 'G' )
+            mult = -1.0;
+
+        double thisRhs = rhs[idxRow] * mult;
+
+        for ( j=0 ; (j<row.getNumElements()) ; ++j )
+        {
+            const int cidx = idx[j];
+
+            if (FIXED_IN_ZERO(cidx))
+                continue;
+
+            if (ctype[cidx])
+                nInts++;
+
+            /* variable which only accepts positive (or zero) value */
+            if ( colLb[cidx] >= -EPS )
+                ++nPos;
+
+            if ( colLb[cidx] <= -EPS )
+                ++nNeg;
+
+            if ( (nNeg>MAX_NEGATIVE_COEFS) || (canYeldLargeValue(coefs[j],colLb[cidx],colUb[cidx])) )
+                break;
+
+            const double neg = mostNegativeContribution( coefs[j], sense[idxRow], colLb[cidx], colUb[cidx] );
+
+            minCoef = min( minCoef, coefs[j]*mult );
+            maxCoef = max( maxCoef, coefs[j]*mult );
+
+            thisRhs -= neg;
+        } /* all columns */
+
+        /* if there are not enough integers or if some condition
+         * triggered premature skip for this row*/
+        if ( (j<row.getNumElements()) || (nInts<=1) )
+            continue;
+
+        /* special case (to ignore), when all variables are fixed to zero */
+        if ( (fabs(thisRhs)<=EPS) && DBL_EQUAL(maxCoef,minCoef)
+                && DBL_EQUAL(maxCoef,1.0) )
+            continue;
+
+        /* special case: ready to use cliques */
+        if ( DBL_EQUAL( minCoef, maxCoef ) &&  DBL_EQUAL( maxCoef, thisRhs ) &&
+                (minCoef>EPS) && ((sense[idxRow]=='E') || (sense[idxRow]=='L'))
+                && (nInts==row.getNumElements()) && (nPos==row.getNumElements()) )
+        {
+        	//printf("Row: %s \t special case: ready to use cliques!\n", lp->getRowName(idxRow).c_str());
+            activeReadyToUse++;
+            processClique( row.getNumElements(), (const int *)idx, cgraph, cvec, colLb, colUb );
+        }
+        /* another special case: x1 + x2 + ... + xn {>=, =} n - 1*/
+        else if ( DBL_EQUAL( minCoef, maxCoef )
+        		&& ((DBL_EQUAL( maxCoef, 1.0 ) && (sense[idxRow]=='E')) || 
+        		   (DBL_EQUAL( maxCoef, -1.0 ) && (sense[idxRow]=='G')))
+    			&& DBL_EQUAL( rhs[idxRow], ((double)(row.getNumElements()-1)) )
+            	&& (nInts==row.getNumElements()) && (nPos==row.getNumElements()) )
+        {
+        	int compIdxs[row.getNumElements()];
+        	for(int i = 0; i < row.getNumElements(); i++)
+        		compIdxs[i] = idx[i] + nCols;
+        	processClique( row.getNumElements(), (const int *)compIdxs, cgraph, cvec, colLb, colUb );
+        	//printf("Row: %s \t special case: ready to use cliques (complement of variables)!\n", lp->getRowName(idxRow).c_str());
+            inactiveReadyToUse++;
+        }
+        else
+        {
+            /* normal case, checking pairwise conflicts
+             * (these are computed increasing RHS in thisRhs) */
+            int j1, j2, cm1 = row.getNumElements()-1;
+            for ( j1=0 ; (j1<cm1) ; ++j1 )
+            {
+                const int cidx1 = idx[j1];
+
+                if ((!ctype[cidx1])||(FIXED_IN_ZERO(cidx1)))
+                    continue;
+
+                const double pos1 = unitaryContribution( coefs[j1], sense[idxRow], colLb[cidx1], colUb[cidx1] );
+
+                end = clock();
+                cpuTime = ((double) (end - start)) / CLOCKS_PER_SEC;
+                if(cpuTime > 60.0)
+                {
+                    success = 0;
+                    break;
+                }
+
+                /* variable cannot be activated anyway, this is the case of integer variables
+                 * with strictly positive lower bounds, e.g. miplib 2010 30n20b8, variable s4 */
+                if ( pos1-EPS >= thisRhs )
+                    continue;
+
+                for ( j2=j1+1 ; (j2<row.getNumElements()) ; ++j2 )
+                {
+                    const int cidx2 = idx[j2];
+
+                    if ((!ctype[cidx2]) || (FIXED_IN_ZERO(cidx2)))
+                        continue;
+
+                    const double pos2 = unitaryContribution( coefs[j2], sense[idxRow], colLb[cidx2], colUb[cidx2] );
+
+                    if ( pos2-EPS >= thisRhs )
+                        continue;
+
+                    end = clock();
+                    cpuTime = ((double) (end - start)) / CLOCKS_PER_SEC;
+                    if(cpuTime > 60.0)
+                    {
+                        success = 0;
+                        break;
+                    }
+
+                    /* recalculating thisRhs, excluding fixed variables */
+                    const double neg1 = mostNegativeContribution( coefs[j1], sense[idxRow], colLb[cidx1], colUb[cidx1] );
+                    const double neg2 = mostNegativeContribution( coefs[j2], sense[idxRow], colLb[cidx2], colUb[cidx2] );
+                    const double newThisRhs = thisRhs + neg1 + neg2;
+
+                    if (pos1+pos2>newThisRhs+0.001)
+                    {
+                        cvec.push_back( pair<int,int>(cidx1,cidx2) );
+                        activePairwise++;
+#ifdef DEBUG_CONF
+                        newConflicts++;
+                        confS.push_back( pair<int,int>(cidx1,cidx2) );
+#endif
+                    }
+
+#ifdef CONSIDER_BINARY_COMPLEMENT
+                    if (pos1>newThisRhs+0.001) /* cidx1 = 1 and cidx2 = 0 */
+                    {
+                        cvec.push_back( pair<int,int>(cidx1,cidx2+nCols) );
+                        mixedPairwise++;
+#ifdef DEBUG_CONF
+                        newConflicts++;
+                        confS.push_back( pair<int,int>(cidx1,cidx2+nCols) );
+#endif
+                    }
+
+                    if (pos2>newThisRhs+0.001) /* cidx1 = 0 and cidx2 = 1 */
+                    {
+                        cvec.push_back( pair<int,int>(cidx1+nCols,cidx2) );
+                        mixedPairwise++;
+#ifdef DEBUG_CONF
+                        newConflicts++;
+                        confS.push_back( pair<int,int>(cidx1+nCols,cidx2) );
+#endif
+                    }
+
+                    if (newThisRhs<-EPS) /* cidx1 = 0 and cidx2 = 0 */
+                    {
+                        cvec.push_back( pair<int,int>(cidx1+nCols,cidx2+nCols) );
+                        inactivePairwise++;
+#ifdef DEBUG_CONF
+                        newConflicts++;
+                        confS.push_back( pair<int,int>(cidx1+nCols,cidx2+nCols) );
+#endif
+                    }
+#endif /* CONSIDER_BINARY_COMPLEMENT */
+                }
+                fetchConflicts( cvec, false, cgraph, neighs );
+            }
+        }
+#ifdef DEBUG_CONF
+        if (newConflicts)
+        {
+            printf("%s,%d,%c,%g : ", lp->getRowName(idxRow).c_str(), nNeg, sense[idxRow], rhs[idxRow]);
+            for ( int cc=0 ; (cc<confS.size()) ; ++cc )
+            {
+                const int var1 = confS[cc].first, var2 = confS[cc].second;
+                printf("(%s, %s) ", var1 < nCols ? lp->getColName(var1).c_str() : ("¬" + lp->getColName(var1 - nCols)).c_str(),
+                                    var2 < nCols ? lp->getColName(var2).c_str() : ("¬" + lp->getColName(var2 - nCols)).c_str() );
+            }
+            printf("\n");
+        }
+#endif
+#undef FIXED_IN_ZERO
+        clock_t rowEnd = clock();
+        double rowTime = ((double) (rowEnd - rowStart)) / CLOCKS_PER_SEC;
+
+        if(rowTime > maxRowTime)
+        {
+            maxRowTime = rowTime;
+            maxRowTimeIdx = idxRow;
+        }
+    }
+
+    fetchConflicts( cvec, true, cgraph, neighs );
+
+    end = clock();
+
+    //double ftime = ((double(end-start))/((double)CLOCKS_PER_SEC));
+    //printf("osi_cgraph took %.3f seconds.\n", ftime);
+
+    return cgraph;
+}
+
+void processClique( const int n, const int *idx, CGraph *cgraph, vector< pair<int, int> > &cvec, const double *colLb, const double *colUb )
+{
+    if ( n >= MIN_CLIQUE_ROW )
+    {
+        vector< int > vidx;
+        vidx.resize(n);
+
+        memcpy( &(vidx[0]), idx, sizeof(int)*n );
+
+        cgraph_add_clique( cgraph, &(vidx[0]), n );
+    }
+    else
+    {
+        int i1, i2, nm1 = n-1;
+        int nCols = cgraph_size(cgraph);
+
+#ifdef CONSIDER_BINARY_COMPLEMENT
+        nCols = nCols / 2;
+#endif /* CONSIDER_BINARY_COMPLEMENT */
+
+        for ( i1=0 ; (i1<nm1) ; ++i1 )
+        {
+        	if ( fabs( colUb[idx[i1]%nCols] <= EPS ) && fabs( colLb[idx[i1]%nCols] <= EPS ) )
+                continue;
+            for ( i2=i1+1 ; (i2<n) ; ++i2 )
+            {
+            	if ( fabs( colUb[idx[i2]%nCols] <= EPS ) && fabs( colLb[idx[i2]%nCols] <= EPS ) )
+                continue;
+                cvec.push_back( pair<int,int>(idx[i1],idx[i2]) );
+            }
+        }
+    }
+}
+
+void fetchConflicts( vector< pair<int,int> > &cvec, const bool lastTime, CGraph *cgraph, vector<int> &neighs )
+{
+    if ( (!lastTime) && (cvec.size()<CVEC_FLUSH) )
+        return;
+
+    if ( cvec.size() == 0 )
+        return;
+    pair< int, int > last;
+    int currNode;
+    vector< pair<int,int> >::const_iterator vIt;
+
+    cvec.push_back( pair<int,int>( INT_MAX, INT_MAX ) );
+
+    neighs.clear();
+    sort( cvec.begin(), cvec.end() );
+    currNode = cvec.begin()->first;
+    last = pair< int,int >( -1, -1 );
+
+    for ( vIt=cvec.begin() ; (vIt!=cvec.end()) ; last=*vIt,++vIt )
+    {
+        if (last == *vIt)  // skipping repetitions
+            continue;
+
+        if ( vIt->first!=currNode )
+        {
+            if (neighs.size())
+            {
+                cgraph_add_node_conflicts_no_sim( cgraph, currNode, &(neighs[0]), neighs.size() );
+                neighs.clear();
+            }
+            currNode = vIt->first;
+        }
+        neighs.push_back( vIt->second );
+    }
+
+    neighs.clear();
+    sort( cvec.begin(), cvec.end(), sort_sec_pair() );
+    currNode = cvec.begin()->second;
+    last = pair< int,int >( -1, -1 );
+    for ( vIt=cvec.begin() ; (vIt!=cvec.end()) ; last=*vIt,++vIt )
+    {
+        if (last == *vIt)  // skipping repetitions
+            continue;
+
+        if ( vIt->second!=currNode )
+        {
+            if (neighs.size())
+            {
+                cgraph_add_node_conflicts_no_sim( cgraph, currNode, &(neighs[0]), neighs.size() );
+                neighs.clear();
+            }
+            currNode = vIt->second;
+        }
+        neighs.push_back( vIt->first );
+    }
+
+    cvec.clear();
+}
+
+
+double mostNegativeContribution( const double coef, const char s, const double colLb, const double colUb )
+{
+    double mult = 1.0;
+
+    if (s=='G')
+        mult = -1.0;
+
+    double c = coef*mult;
+
+    return min( c*colLb, c*colUb );
+}
+
+double unitaryContribution( const double coef, const char s, const double colLb, const double colUb )
+{
+    if ( colUb <= EPS )
+        return 0.0;
+
+    if (s=='G')
+        return coef*-1.0;
+    else
+        return coef;
+
+    // some dumb compilers print warnings if
+    // function does not have a return outside ifs
+    return coef;
+}
+
+bool canYeldLargeValue( const double coef, const double colLb, const double colUb )
+{
+    if ( fabs( coef ) >= LARGE_CONST )
+        return true;
+
+    if ( fabs( colLb ) >= LARGE_CONST )
+        return true;
+
+    if ( fabs( colUb ) >= LARGE_CONST )
+        return true;
+
+    if ( fabs(colUb)*fabs(coef) >= LARGE_CONST )
+        return true;
+
+    if ( fabs(colLb)*fabs(coef) >= LARGE_CONST )
+        return true;
+
+    return false;
+}
+
+int compute_row_priority( OsiSolverInterface *lp, const int r )
+{
+    int nInteger = 0;
+    int nContinuous = 0;
+
+    const CoinPackedMatrix *M = lp->getMatrixByRow();
+    const CoinShallowPackedVector &row = M->getVector(r);
+    const int *idx = row.getIndices();
+    const double *coefs = row.getElements();
+    int nPositive = 0;
+    int nNegative = 0;
+    const char sense = lp->getRowSense()[r];
+    const double rhs = lp->getRightHandSide()[r];
+    double minCoef = DBL_MAX;
+    double maxCoef = -DBL_MAX;
+
+    const int n = row.getNumElements();
+    bool allSameSense;
+    int result = 0, i;
+
+    if ( n == 1 )
+    {
+        result = 10000;
+        goto END;
+    }
+
+    for ( i = 0; (i<n) ; i++)
+    {
+        if (lp->isInteger(idx[i]))
+            nInteger++;
+        else
+            nContinuous++;
+
+        if (coefs[i]>EPS)
+            nPositive++;
+        else
+            nNegative++;
+
+        minCoef = std::min( minCoef, coefs[i] );
+        maxCoef = std::max( maxCoef, coefs[i] );
+    }
+
+    allSameSense = ((!nPositive)||(!nNegative));
+
+    /* checking if this is a "perfect" set packing constraint  */
+    if ((
+                ( (sense=='L')||(sense=='E') ) &&
+                ( nInteger==row.getNumElements() ) &&
+                ( allSameSense ) &&
+                ( nPositive ) &&
+                ( DBL_EQUAL( minCoef, maxCoef) ) &&
+                ( DBL_EQUAL( rhs, maxCoef) )
+            ))
+        result += std::max( (1000 - n), 10 );
+    else
+        result += 5000;
+
+    /* definitely a constraint were conflicts cannot be extracted */
+    if ( (!nNegative) && (sense=='G') )
+        result += 6000;
+
+    if (nContinuous)
+    {
+        if (nContinuous < 10)
+            result += 100*nContinuous;
+        else
+        {
+            result = oo;
+            goto END;
+        }
+    }
+
+    if (!allSameSense)
+    {
+        result += 100;
+
+        /* probably a flow conservation constraint if
+           there are many coefficients with both signs */
+        if ( (nPositive > 1) and (nNegative > 1) )
+            result += min( nPositive, nNegative ) * 1000;   // if both are high values, penalizing
+
+        if ( (nPositive>5) && (nNegative>5) )
+        {
+            result = oo/2;
+            goto END;
+        }
+    }
+
+END:
+    return result;
+}
+
---
+++ src/writeclqw backup.cpp
@@ -0,0 +1,247 @@
+#include <cassert>
+#include <cstdio>
+#include <algorithm>
+#include <climits>
+#include <cfloat>
+#include <cstdlib>
+#include <vector>
+#include <OsiClpSolverInterface.hpp>
+#include <CglClique.hpp>
+#include "osi_cgraph.h"
+
+extern "C"
+{
+#include "memory.h"
+#include "strUtils.h"
+#include "cgraph.h"
+#include "conflict_discover.h"
+#include "clique_separation.h"
+}
+#include "osi_cgraph.h"
+
+using namespace std;
+
+#define MIN_VIOLATION 0.02
+
+/* minimum fractional part to a variable to be considered fractional */
+#define MIN_FRAC      0.001
+
+#define MAX( a, b ) ( (a)>(b) ? (a) : (b) )
+#define MIN( a, b ) ( (a)<(b) ? (a) : (b) )
+
+void readLP( const char *fileName, OsiSolverInterface *solver );
+
+double fracPart( const double x );
+
+
+int main( int argc, char **argv )
+{
+    OsiSolverInterface *solver = NULL;
+
+    OsiClpSolverInterface *realSolver = new OsiClpSolverInterface();
+
+    solver = (OsiSolverInterface*) realSolver;
+
+    readLP( argv[1], solver );
+
+    printf("writeclqw\n\n");
+    char problemName[ 256 ];
+    getFileName( problemName, argv[1] );
+    printf("loaded %s \n", problemName );
+    printf("\t%d variables (%d integer) %d rows %d nz\n\n", solver->getNumCols(), solver->getNumIntegers(), solver->getNumRows(), solver->getNumElements() );
+
+    clock_t startOG = clock();
+    CGraph *cgraph = osi_build_cgraph( solver );
+    clock_t endOG = clock();
+    const double timeOG = ((double)(endOG-startOG)) / ((double)CLOCKS_PER_SEC);
+
+    cgraph_recompute_degree( cgraph );
+
+    unsigned long int m = 0;
+    {
+        int i;
+        for ( i=0 ; (i<cgraph_size(cgraph)) ; ++i )
+            m+=cgraph_degree(cgraph,i);
+    }
+
+    /*
+    printf("original cgraph dimensions: n: %d m: %lu t: %.3f\n", cgraph_size(cgraph), m, timeOG );
+
+    {
+       char oname[256];
+       sprintf( oname, "%s_o.clwq", problemName );
+
+       cgraph_save( cgraph, oname );
+    }*/
+#ifdef DEBUG_CONF
+    // check new way to iterate through nodes
+    {
+        int i;
+        int nNodesInCliques = 0;
+        int *neighs = (int*) xmalloc( cgraph_size(cgraph)*1000 );
+        int *costs = (int*) xmalloc( cgraph_size(cgraph)*sizeof(int) );
+        for ( i=cgraph_size(cgraph)-1 ; (i>=0) ; --i )
+            costs[i] = cgraph_size(cgraph) - i + 5;
+        int nneighs = 0;
+        NeighIterator *nit = nit_create();
+        printf("checking...\n");
+        fflush(stdout);
+        for ( i=0 ; (i<cgraph_size(cgraph)) ; ++i )
+        {
+            //printf("node %d degree %d \n" , i, cgraph_degree(cgraph, i));
+            fflush( stdout );
+            fflush( stderr );
+            nneighs = cgraph_get_all_conflicting( cgraph, i, neighs, cgraph_size(cgraph)*1000 );
+            nit_start( nit, cgraph, i, costs );
+            int *p = neighs+nneighs-1;
+            int n;
+            int nel = 0;
+            int j=0;
+
+            /*
+            printf("\n\n");
+            fflush(stdout); fflush(stderr);
+            printf("NEIGHS:");
+            for ( j=0 ; (j<nneighs ) ; ++j )
+               printf(" %d", neighs[j]);
+            fflush( stdout );  fflush( stderr );
+            printf("\n\nNEIG Its:");
+            */
+
+            p = neighs+nneighs-1;
+            while ( (n=nit_next(nit))!=INT_MAX )
+            {
+                //printf(" %d", n);
+                //         fflush( stdout );  fflush( stderr );
+                //printf("[%d %d %d %d ]\n", j, nneighs-j, *p, n ); fflush(stdout); fflush(stderr);
+                ++j;
+                assert(  (p>=neighs)  );
+                assert( p<neighs+nneighs );
+                assert( *p == n );
+                ++nel;
+
+                --p;
+                fflush( stdout );
+                fflush( stderr );
+            }
+            assert( nel == nneighs );
+            //printf(" nneighs %d nel %d \n", nneighs, nel );  fflush(stdout);
+        }
+
+        nit_free( &nit );
+        free( neighs );
+        free( costs );
+    }
+#endif
+
+    CliqueSeparation *clqSep = NULL;
+
+    printf("solving relaxation ... ");
+    clock_t start = clock();
+    solver->initialSolve();
+    printf("Initial dual bound %g\n", solver->getObjValue() );
+    clock_t end = clock();
+    printf("solved in %.3f\n", ((double)(end-start)) / ((double)CLOCKS_PER_SEC) );
+    clock_t startSep, endSep;
+    double timeSep;
+
+    cgraph_print_summary( cgraph, "original conflict graph" );
+
+    clock_t tend = clock();
+    double totalTime = ((double)(tend-start)) / ((double)CLOCKS_PER_SEC);
+    printf("\nEnd of root node relaxation. Dual limit: %g time: %.3f\n", solver->getObjValue(), totalTime );
+
+#ifdef CONSIDER_BINARY_COMPLEMENT
+    goto WRITE_GRAPH;
+#endif /* CONSIDER_BINARY_COMPLEMENT */
+
+    {
+        int i, idx = 0;
+        const double *x = solver->getColSolution();
+
+        int *iv = new int[ solver->getNumCols() ];
+        for ( i=0 ; i<solver->getNumCols() ; ++i )
+            iv[i] = -1;
+
+        for ( i=0 ; (i<cgraph_size(cgraph)) ; ++i )
+            if ((cgraph_degree(cgraph, i)<2)||(fracPart(x[i])<MIN_FRAC))
+                continue;
+            else
+                iv[i] = idx++;
+
+        CGraph *newGraph = cgraph_create_induced_subgraph( cgraph, iv );
+
+        int nRemoved = 0;
+        for ( i=0 ; (i<cgraph_size(newGraph)) ; ++i )
+        {
+            if (cgraph_degree(newGraph, i)<2)
+            {
+                iv[cgraph_get_original_node_index(newGraph,i)] = -1;
+                ++nRemoved;
+            }
+        }
+        if (nRemoved)
+        {
+            /* updating iv */
+            idx = 0;
+            for ( i=0 ; (i<cgraph_size(cgraph)); ++i )
+                if (iv[i] != -1 )
+                    iv[i] = idx++;
+
+            cgraph_free( &newGraph );
+            newGraph = cgraph_create_induced_subgraph( cgraph, iv );
+        }
+
+        /* removing from this graph all nodes with degree <= 1 */
+
+
+        for ( i=0 ; (i<cgraph_size(newGraph)) ; ++i )
+            cgraph_set_node_weight( newGraph, i, cgraph_weight(x[cgraph_get_original_node_index(newGraph,i)]) );
+        cgraph_free( &cgraph );
+        cgraph = newGraph;
+
+        delete[] iv;
+    }
+
+    WRITE_GRAPH:
+
+    cgraph_print_summary( cgraph, "Conflict graph" );
+
+    if ( cgraph_size( cgraph ) == 0 )
+    {
+        printf("EMPTY conflict graph. exiting...\n");
+        exit(0);
+    }
+
+    char fileName[256];
+    getFileName( fileName, argv[1] );
+    printf("\nFile name: %s\n", fileName );
+    char outName[256];
+    sprintf( outName, "%s.clqw", fileName );
+
+    cgraph_save( cgraph, outName );
+
+    cgraph_free( &cgraph );
+
+    delete realSolver;
+
+    return EXIT_SUCCESS;
+}
+
+void readLP( const char *fileName, OsiSolverInterface *solver )
+{
+    solver->setIntParam(OsiNameDiscipline, 2);
+    solver->readMps( fileName );
+    //solver->readLp( fileName );
+    solver->setIntParam(OsiNameDiscipline, 2);
+    solver->messageHandler()->setLogLevel(1);
+    solver->setHintParam(OsiDoReducePrint,true,OsiHintTry);
+}
+
+double fracPart( const double x )
+{
+    double nextInteger = ceil( x );
+    double previousInteger = floor( x );
+
+    return MIN( nextInteger-x, x-previousInteger );
+}
