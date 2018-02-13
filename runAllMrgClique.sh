2ef18a63cf4c3b07aa95d7c1e67468d8fda13c15
Haroldo <haroldo.santos@gmail.com>
2018-02-13 13:40:59 -0200

    test scripts

--- runAllMrgClique.sh
+++ runAllMrgClique.sh
@@ -1,13 +1,41 @@
+rm summary.csv
 for file in ~/inst/cbcbench/*.mps.gz;
 do
     inst=`basename $file .mps.gz`
     echo -n "$inst," >> summary.csv
-    ./bin/opt/mrgclq $file 2 1 1
+    ./Release/mrgclq $file 2 1 1
 done
 for file in ~/inst/bpconf/lp/*.lp;
 do
     inst=`basename $file .lp`
     echo -n "$inst,"  >> summary.csv
-    ./bin/opt/mrgclq $file 2 1 1
+    ./Release/mrgclq $file 2 1 1
 done
+mv summary.csv summary-2clq.csv
+for file in ~/inst/cbcbench/*.mps.gz;
+do
+    inst=`basename $file .mps.gz`
+    echo -n "$inst," >> summary.csv
+    ./Release/mrgclq $file 1 1 1
+done
+for file in ~/inst/bpconf/lp/*.lp;
+do
+    inst=`basename $file .lp`
+    echo -n "$inst,"  >> summary.csv
+    ./Release/mrgclq $file 1 1 1
+done
+mv summary.csv summary-1clq.csv
+for file in ~/inst/cbcbench/*.mps.gz;
+do
+    inst=`basename $file .mps.gz`
+    echo -n "$inst," >> summary.csv
+    ./Release/mrgclq $file 3 1 1
+done
+for file in ~/inst/bpconf/lp/*.lp;
+do
+    inst=`basename $file .lp`
+    echo -n "$inst,"  >> summary.csv
+    ./Release/mrgclq $file 3 1 1
+done
+mv summary.csv summary-3clq.csv
 
